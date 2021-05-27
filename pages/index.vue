53d873ca778d87b73231b994a806f88f60e6fd4b
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-05-27 17:24:34 +0300

    made the coin page 😄

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,84 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
+    <v-col cols="12" sm="8" md="6" align="center">
+       <v-img src="/logoE.png" contain  max-height="100" max-width="100" class="my-6"/>
+      <v-card >
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Crypto Dashboard
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/exchange"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
+  <v-autocomplete
+      v-model="coins"
+      :disabled="isUpdating"
+      :items="coin"
+      chips
+      clearable
+      hide-details
+      hide-selected
+      item-text="name"
+      item-value="code"
+      label="Search for a coin..."
+      solo
+    >
+      <template v-slot:no-data>
+        <v-list-item>
+          <v-list-item-title>
+            Search for your favorite
+            <strong>Cryptocurrency</strong>
+          </v-list-item-title>
+        </v-list-item>
+      </template>
+      <template v-slot:selection="{ attr, on, item, selected }"
+      >
+      <router-link :to="'coins/'+item.code">
+        <v-chip
+          v-bind="attr"
+          :input-value="selected"
+          color="blue-grey"
+          class="white--text"
+          v-on="on"
+        >
+        
+          <v-icon left>
+            mdi-bitcoin
+          </v-icon>
+          <span v-text="item.name"></span>
+        </v-chip>
+          </router-link >
+      </template>
+      <template v-slot:item="{ item }"  >
+        <v-list-item-avatar
+          color="indigo"
+          class="headline font-weight-light white--text"
+        >
+          {{ item.name.charAt(0) }}
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.name"></v-list-item-title>
+          <v-list-item-subtitle v-text="item.code"></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon>mdi-bitcoin</v-icon>
+        </v-list-item-action>
+      </template>
+    </v-autocomplete>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+<script lang="ts">
+let cc= require('../static/cc.json')
+ export default {
+  head:{
+    title:"main page"
+    },
+    data: () => ({
+      coins: [],
+      coin: cc,
+      isUpdating: false,
+      model: null,
+      search: null,
+      tab: null,
+    }),
   }
-}
 </script>
